5c95f59465c09117dc199232906bf5630c5e3b86
jvftw <jvmbworks@gmail.com>
2023-05-07 15:14:46 -0400

    main.js then & catch

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,12 @@ console.log("Iniciado")
 
 const offset = 0
 const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+fetch(url)
+    .then(function (response){
+    console.log(response)
+    })
+    .catch(function(error){
+        console.error(error)
+    })
