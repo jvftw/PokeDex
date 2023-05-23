a673adb1dbc0bb97dcfabb10f3d7d7a0b6af08c6
jvftw <jvmbworks@gmail.com>
2023-05-22 22:20:00 -0400

    return function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,22 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToHtml(pokemon) {
+    return `   
+        <li class="pokemon">
+        <span class="name">${pokemon.name}</span>
+        <span class="number">#001</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>     
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+`
+    
+}
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
