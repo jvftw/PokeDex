df916f77c15c767f4ce0d5bffd488884c3c204ed
jvftw <jvmbworks@gmail.com>
2023-05-22 22:45:12 -0400

    function api to list

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
     return `   
         <li class="pokemon">
         <span class="name">${pokemon.name}</span>
@@ -17,16 +17,18 @@ function convertPokemonToHtml(pokemon) {
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
         </div>
     </li>
-`
-    
+`   
 }
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            console.log(convertPokemonToLi(pokemon))
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
         }   
     })
     .catch((error) => console.error(error))
