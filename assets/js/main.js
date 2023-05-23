295d39020483572d65fc956cb01d3459030c3189
jvftw <jvmbworks@gmail.com>
2023-05-22 22:14:22 -0400

    debugger

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,8 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        debugger    
+        console.log(pokemonList)})
     .catch((error) => console.error(error))
